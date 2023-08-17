90374bb283c105e633b3bfd7c55f9854ec6bfcc7
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-08-17 15:05:18 +0000

    Third Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,27 @@
 Rails.application.routes.draw do
 
-  devise_for :customers
-  devise_for :admins
+ devise_for :customers, :controllers => {
+  :sessions => 'customers/sessions',
+  :passwords => 'customers/passwords',
+  :registrations => 'customers/registrations',
+  }
+
+  devise_for :admins, :controllers => {
+  :sessions => 'admins/sessions',
+  :passwords => 'admins/passwords',
+  :registrations => 'admins/registrations',
+  }
+
+  root to: "homes#top"
 
   namespace :admin do
-    get 'orders/show'
+    resources :orders, only: [:show]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :homes, only: [:top, :about]
   end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :admin do
-    get 'sessions/new'
-  end
+
   namespace :public do
     get 'addresses/index'
     get 'addresses/edit'
