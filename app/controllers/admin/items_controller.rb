5e188e56215b3183bb0f2c1d5e227b619c6c0205
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-08-19 06:17:53 +0000

    [Add]admin_item_new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,7 +3,16 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+    redirect_to admin_item_path
+    else
+      flash[:item_created_error] = "商品情報が正常に保存されませんでした。"
+    end
 
   def show
   end
