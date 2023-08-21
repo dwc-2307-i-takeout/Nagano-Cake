ba1f8f825a5b601be0391b18e609265679af18d3
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-08-21 08:22:11 +0000

    commit1

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,17 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart = Cart.new
+    @carts = current_customer.cart_items
   end
+
+  def create
+  end
+
+
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:customer_id, :item_id, :quantity)
+  end
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,11 @@ class CartItem < ApplicationRecord
 
   belongs_to :customer
   belongs_to :item
+  
+  validates :quantity, presence: true
+  
+  def total_price
+    item.tax_included * quantity
+  end
 
 end
