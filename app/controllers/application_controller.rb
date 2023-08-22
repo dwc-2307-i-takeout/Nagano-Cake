aa9bfd49d2bc4e261e335f6337b8d1629da39ee7
nbjun1 <138277763+nbjun1@users.noreply.github.com>
2023-08-22 05:15:47 +0000

    14 2208

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Customer)
+      root_path
+    elsif resource.is_a?(Admin)
+      admin_admin_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    if  resource.is_a?(Customer)
+      root_path
+    elsif resource.is_a?(Admin)
+      new_admin_session_path
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 end
