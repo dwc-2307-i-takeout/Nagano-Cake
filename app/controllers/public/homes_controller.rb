bbbb56d6ff1ff22bfc4277d4f56025227f276954
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-08-23 15:57:35 +0000

    fix top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,10 +1,11 @@
 class Public::HomesController < ApplicationController
+
   def top
     @genres = Genre.all
-    @items=Item.all.order(created_at: :desc).limit(4)
-
+    @items=Item.order(updated_at: "DESC").limit(4)
   end
 
   def about
   end
+
 end
