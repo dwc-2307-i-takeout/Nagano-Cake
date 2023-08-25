abd3c093ff341674325a2b987d335951c3543734
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-08-25 08:06:18 +0000

    admin_customers_show

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,17 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customer = Customer.page(params[:page]).per(10)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_kana_name, :first_kana_name, :email, :post_number, :address, :phone_number, :is_withdrawal)
   end
 end
