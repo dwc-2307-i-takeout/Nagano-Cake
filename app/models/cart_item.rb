4aeddd7d4e17cec35e98da018c29671dc0cff01b
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-08-26 08:27:57 +0000

    commit1

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,15 +2,11 @@ class CartItem < ApplicationRecord
 
   belongs_to :customer
   belongs_to :item
-  
+
   validates :quantity, presence: true
-  
+
   def total_price
     item.tax_included * quantity
   end
-
-def sum_price 
-  item.taxin_price*quantity
-end
 
 end
