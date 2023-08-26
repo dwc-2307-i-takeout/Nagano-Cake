8aced6eeaa58d852962b4856749c670fa2b30b56
EC2 Default User <ec2-user@ip-172-31-11-114.ap-northeast-1.compute.internal>
2023-08-26 08:27:01 +0000

    Admin-Order-history

---
+++ app/controllers/admin/order_details_controller.rb
@@ -0,0 +1,13 @@
+class Admin::OrderDetailsController < ApplicationController
+  def update
+    @order = OrderDetail.find(params[:id])
+    @order.update(orderdetail_params)
+    redirect_to admin_order_path(@order.order.id)
+  end
+
+  private
+
+  def orderdetail_params
+    params.require(:order_detail).permit(:making_status)
+  end
+end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,15 +1,26 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
   
   def index
     @orders = Order.all
   end
   
+   def update
+      @order = Order.find(params[:id])
+     if @order.update(order_params)
+      redirect_to  admin_order_path(@order)
+     else
+      render :show
+     end
+   end
+  
   private
 
   def order_params
-    params.require(:order).permit(:name, :address, :billing_amount, :post_number, :postage)
+    params.require(:order).permit(:name, :address, :billing_amount, :post_number, :postage, :status)
   end
 
   def address_params
