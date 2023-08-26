d63e6ce12364aa039c8f9b898b81598fc4efeb4f
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-08-26 16:18:24 +0000

    commit2

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -9,7 +9,4 @@ class Admin::HomesController < ApplicationController
     params.require(:order).permit(:name, :address, :billing_amount, :post_number, :postage)
   end
 
-  def address_params
-    params.require(:order).permit(:name, :address)
-  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,12 @@ class Address < ApplicationRecord
 
   belongs_to :customer
 
+  validates :post_number, presence: true, length: { is:7}
+  validates :address, presence: true
+  validates :name, presence: true
+
+  def customer_address
+    "〒" + " " + post_number + " " + address + " " + name
+  end
+
 end
