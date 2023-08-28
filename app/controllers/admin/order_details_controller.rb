accdbd2b7abf6af6a4c3505679032c0b0eb9af70
nbjun1 <138277763+nbjun1@users.noreply.github.com>
2023-08-28 05:06:42 +0000

    fix

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,11 +1,10 @@
 class Admin::OrderDetailsController < ApplicationController
   
-  def update
+    def update
     @order = OrderDetail.find(params[:id])
     @order.update(orderdetail_params)
     redirect_to admin_order_path(@order.order.id)
   end
-  
   private
 
   def orderdetail_params
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -39,24 +39,6 @@ class Admin::OrdersController < ApplicationController
     end
   end
 
-  def confirm
-    @order = Order.find(params[:id])
-    @total = @order.calculate_total_price
-
-    # フォームの送信に必要な情報を設定
-    @order.name = params[:order][:name]
-    @order.address = params[:order][:address]
-    @order.post_number = params[:order][:post_number]
-    @order.postage = params[:order][:postage]
-    @order.billing_amount = params[:order][:billing_amount]
-
-    # ビュー内でフォームに表示するためのインスタンス変数を設定
-    @cart_items = @order.cart_items
-    @all_price = @order.calculate_all_price
-
-    render :confirm
-  end
-
   private
 
   def order_params
